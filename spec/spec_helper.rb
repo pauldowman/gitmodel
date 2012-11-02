2c0cf67606b1a93ae53244f66c3e0adb2d5ee2c8
Kyle Brett <kyle@kylebrett.com>
2012-11-02 09:50:14 -0700

    RSpec run filtering + random example order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,10 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |c|
   c.mock_with :rspec
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.order = :random
 end
 
 class TestEntity
